811120d10ef8991e86a7200589a1b25ac4df6dcb
GhanKr <ghanshyam2523@gmail.com>
2022-10-22 13:33:52 +0530

    Remove Syntax error

--- script/toolScript.js
+++ script/toolScript.js
@@ -1,11 +1,10 @@
-let barIcon=document.querySelector('.menuBar');
-let tools=document.querySelector('.tools');
-let barChild=barIcon.children[0];
-barChild.addEventListener('click',() => {
-    barChild.addEventListener('click',() => {
-        barChild.classList.toggle('fa-bars');
-        barChild.classList.toggle('fa-circle-xmark');
-        tools.classList.toggle('none');
-        tools.classList.toggle('active');
+let barIcon = document.querySelector('.menuBar');
+let tools = document.querySelector('.tools');
+let barChild = barIcon.children[0];
+barChild.addEventListener('click', () => {
+    barChild.classList.toggle('fa-bars');
+    barChild.classList.toggle('fa-circle-xmark');
+    tools.classList.toggle('none');
+    tools.classList.toggle('active');
 });
 
